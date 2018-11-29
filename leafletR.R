424341183d56b029445e61e1499260988949103a
anoobvinu07 <42816154+anoobvinu07@users.noreply.github.com>
2018-11-29 11:52:10 -0500

    leaflet_addons

--- leafletR.R
+++ leafletR.R
@@ -1,83 +1,96 @@
-install.packages("leaflet") # main package 
+# Leaflet for R
+# November 29, 2018
+# AnB
 
-install.packages("maps")
+# easily make interactive maps within R
+# used by many to publish maps
+# easily render spatial objects from the sp and sf packages or dataframes with lat/long column
 
-
+# basic example
 library(leaflet)
-
-# basics
-m <- leaflet() %>%
-  addTiles() %>%  # Add default OpenStreetMap map tiles
-  addMarkers(lng=-73.2013454, lat=44.4783732, popup="Waterman building, UVM") # Decimal degrees - latitude and longitude, the values are bounded by ±90° and ±180° respectively.
-
-print(m)  
-
-# map view
-
-#setView()
-m %>% setView(-73.2, 44.48, zoom = 16) #Restricts the map view to the given bounds
-
-#fitBounds()
-m %>% fitBounds(-73.2, 44.48,-73.1, 44.49) #Set the bounds of a map
-
-#setMaxBounds() and clearBounds() similar functions
-
-#Map widget
 library(maps)
-mapStates = map("state", fill = TRUE, plot = FALSE)
 
-leaflet(data = mapStates) %>% addTiles() %>%
-  addPolygons(fillColor = topo.colors(10, alpha = NULL), stroke = FALSE)
+# basic usage
+#1. create a map widget by calling leaflet()
+#2. add layers to maps using layer functions to modify the map widget
+#3. repeat step 2 as desired
+#print the map widget
 
-##Basemaps
-#The easiest way to add tiles is by calling addTiles() with no arguments; by default, OpenStreetMap tiles are used.
-m <- leaflet() %>% setView(lng=-73.2013454, lat=44.4783732, zoom = 12)
-m %>% addTiles() #default:OpenStreetMap
+#basic example
+m <- leaflet() %>%
+  addTiles() %>%
+  addMarkers(lng = -73.2013, lat = 44.4783, popup = "landmark", label= "landmark")
+m
 
-#Third party tiles or maps
-names(providers) # list of third party map providers
-#examples
+m <- leaflet(options = leafletOptions(minZoom = 12, maxZoom = 18)) %>%
+  addTiles() %>%
+  addMarkers(lng = -73.2013, lat = 44.4783, popup = "landmark", label= "landmark")
+m
+
+# views
+m <- leaflet %>% addTiles()
+m %>% setView(-73.2, 44.48, zoom = 16)
+
+m %>% fitBounds(-73.2, 44.48,-73.1, 44.49)
+
+m %>% setMaxBounds(-73.2, 44.48,-73.1, 44.49)
+
+m %>% clearBounds()
+
+# dataobjects
+# base R
+df <- data.frame(Lat=1:10,Long=rnorm(10))
+leaflet(df) %>% addTiles() %>% addCircles()
+
+# maps package
+library(maps)
+mapStates <- map("state",fill=TRUE,plot=FALSE)
+leaflet(mapStates) %>% addTiles() %>% 
+  addPolygons(fillColor = topo.colors(10, alpha=NULL), stroke=FALSE)
+
+# Basemaps
+m
+m %>% addTiles()
+names(providers)
+
 m %>% addProviderTiles(providers$Stamen.Toner)
-m %>% addProviderTiles(providers$Esri.NatGeoWorldMap) #ESRI- reference map includes physical and natural features, administrative boundaries, cities, transportation infrastructure, landmarks, protected areas, ocean floors, and other layers
+m %>% addProviderTiles(providers$Esri.NatGeoWorldMap)
 
-# stacking base maps
+# Stacking maps
+
 m %>% addProviderTiles(providers$Esri.NatGeoWorldMap) %>%
   addProviderTiles(providers$Stamen.TonerLines,
                    options = providerTileOptions(opacity = 0.95)) %>%
   addProviderTiles(providers$Stamen.TonerLabels)
 
-#Shape and Polygons
-#Circles
+# Shapes and polygons
+
+# Circles
 cities <- read.csv(textConnection("
-City,Lat,Long,Pop
-Boston,42.36,-71.05,645966
-New York City,40.71,-74.00,8406000
-Philadelphia,39.95,-75.16,1553000
-"))
+                                  City,Lat,Long,Pop
+                                  Boston,42.36,-71.05,645966
+                                  New York City,40.71,-74,8406000
+                                  Philadelphia,39.95,-75.16,1553000"))
 
 leaflet(cities) %>% addTiles() %>%
-  addCircles(lng = ~Long, lat = ~Lat, weight = 1,
-             radius = ~sqrt(Pop) * 30, popup = ~City
-  )
+  addCircles(lng=~Long,lat=~Lat, weight=1,
+             radius = ~sqrt(Pop)*30, popup = ~City)
 
-#rectangles
-leaflet() %>% addTiles() %>%
+# rectangles
+leaflet() %>% addTiles() %>% 
   addRectangles(
-    lng1=-118.456554, lat1=34.078039,
-    lng2=-118.436383, lat2=34.062717,
-    fillColor = "transparent" #red, blue etc.
+    lng1 = -118.45, lat1 = 34.07,
+    lng2 = -118.43,lat2 = 34.06,
+    fillColor = "red"
   )
 
-#Markers
-data(quakes) #Locations of Earthquakes off Fiji
-
+# Markers
+data(quakes) # locations of earthquakes off fiji
 head(quakes)
+leaflet(data=quakes[1:20,]) %>% addTiles() %>%
+  addMarkers(~long,~lat,popup = ~as.character(mag), label = ~as.character(mag))
 
-# Show first 20 rows from the `quakes` dataset
-leaflet(data = quakes[1:20,]) %>% addTiles() %>%
-  addMarkers(~long, ~lat, popup = ~as.character(mag), label = ~as.character(mag))
-
-#Cluster markers together
 leaflet(quakes) %>% addTiles() %>% addMarkers(
   clusterOptions = markerClusterOptions()
-)
+) %>% addMeasure() %>% addMiniMap()
+
